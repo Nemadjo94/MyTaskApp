d30f9eef6dce38f01a8ba1c9b4ed9015cbd1bfba
Nemanja <nemadjo94@gmail.com>
2019-06-11 16:57:08 +0200

    Update SwapableItem.js

--- components/SwapableItem.js
+++ components/SwapableItem.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
 import Swipeable from 'react-native-swipeable';
-import {deleteTask} from '../App';
 
 export default class SwapableItem extends Component {
 
